7a11cb3c1d3f68dd77ff758bb0380e56f039e703
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-05-23 15:24:36 +0900

    12Week datePicker를 이용하여 선택한 날짜 저장 예제

--- Week12_338/app/build.gradle.kts
+++ Week12_338/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.janob.week12_327"
+    namespace = "com.janob.week12_338"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.janob.week12_327"
+        applicationId = "com.janob.week12_338"
         minSdk = 29
         targetSdk = 33
         versionCode = 1
@@ -39,7 +39,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
